1ab6a12c4f6767deacc66b13045b66a2c62071e6
Marcus0086 <marcgupta42@gmail.com>
2022-12-24 14:11:41 +0530

    Added a Token enmum with Debug and PartialEq traits
    Added all of the regex variants

---
+++ src/lexer/lib.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, PartialEq)]
+enum Token {
+    // Single characters
+    Char(char),
+    AnyChar,
+    ZeroOrMore,
+    OneOrMore,
+    ZeroOrOne,
+    GroupStart,
+    GroupEnd,
+    Alternation,
+
+    // Keywords
+    Dot,
+    Star,
+    Plus,
+    Question,
+    LeftParen,
+    RightParen,
+    Pipe,
+
+    // Character classes
+    CharClassStart,
+    CharClassEnd,
+    CharClassInvert,
+    CharRange,
+
+    // Anchors
+    Anchor_start,
+    anchor_end,
+    anchor_word_boundary,
+
+    // Special characters
+    Escape,
+    Octal,
+    Hex,
+    Unicode,
+}
