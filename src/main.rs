f1dd4bbd3cd8ea6835d38f419abeafc458d178a7
Marcus0086 <marcgupta42@gmail.com>
2022-12-24 15:30:48 +0530

    Created a parse_regex function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use lexer::RegexLexer;
 
 mod lexer;
+
+pub fn parse_regex(regex: &str) -> String {
+    let mut lexer = RegexLexer::new(regex);
+    lexer.explan_regex()
+}
 fn main() {
     let regex = "(abc * | bc+ )";
-    let mut lexer = RegexLexer::new(regex);
-    let result = lexer.explan_regex();
+    let result = parse_regex(regex);
     print!(
-        "The explanation for the entered regex {} is {}",
+        "The explanation for the entered regex {} is: {}",
         regex, result
     );
 }
