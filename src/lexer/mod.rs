50adcd76d8307abd3f9c525fa2830c1efe7719d5
Marcus0086 <marcgupta42@gmail.com>
2022-12-24 14:14:28 +0530

    Implemented RegexLexer
    Added a new method for the RegexLexer which takes the regex as string and saves it in the RegexLexer

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -5,3 +5,14 @@ struct RegexLexer {
     pos: usize,
     chars: Vec<char>,
 }
+
+impl RegexLexer {
+    fn new(regex: &str) -> Self {
+        let chars: Vec<char> = regex.chars().collect();
+        Self {
+            regex: regex.to_string(),
+            pos: 0,
+            chars,
+        }
+    }
+}
